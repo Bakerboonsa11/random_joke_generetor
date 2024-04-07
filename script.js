3c639056a48127732cc9040f80e580f756c0e2c4
bonsa <bonsa>
2024-04-07 23:45:39 +0300

    1

---
+++ script.js
@@ -0,0 +1,30 @@
+const joke_container = document.querySelector(".gift");
+const btn = document.querySelector(".btn");
+const url =
+  "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,sexist";
+
+console.log(joke_container);
+console.log(btn);
+const getgift = () => {
+  fetch(url)
+    .then((data) => {
+      if (!data.ok) throw new Error("something is wrong");
+      return data.json();
+    })
+    .then((item) => {
+      if (item.type === "twopart") {
+        const result = `${item.setup} ${item.delivery}`;
+        joke_container.textContent = `${result}`;
+      } else {
+        console.log(item.joke);
+        joke_container.textContent = `${item.joke}`;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+btn.addEventListener("click", getgift);
+
+// getgift();
